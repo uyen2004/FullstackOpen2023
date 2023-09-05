071dfed1f4e41cabc40a6954078d72dd00d7439d
e2000592 <e2000592@vamk.fi>
2023-09-05 22:25:04 +0300

    Exc1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 
+const Statistics = (props) => {
+  const total = props.bad + props.good + props.neutral;
+  return(
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>average {(props.good + props.bad * -1) / total}</p>
+      <p>positive {(props.good/ total)*100}% </p>
+    </div>
+    )
+}
+
 const App = () => {
  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = bad + good + neutral
+  
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -13,11 +26,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>average {(good + bad * -1) / total}</p>
-      <p>positive {(good/ total)*100}% </p>
+      <Statistics good = {good} neutral ={neutral} bad ={bad} />
     </div>
   )
 }
