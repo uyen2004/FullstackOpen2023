67879870524b2847bd6d49ac8188016e67e523bc
e2000592 <e2000592@vamk.fi>
2023-09-05 22:19:30 +0300

    Exc1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+  const total = bad + good + neutral
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -16,6 +16,8 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>average {(good + bad * -1) / total}</p>
+      <p>positive {(good/ total)*100}% </p>
     </div>
   )
 }
