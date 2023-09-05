83f3835aed662541aea97d3f0c003ba20bd7f331
e2000592 <e2000592@vamk.fi>
2023-09-06 01:59:41 +0300

    Exc2.2-2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -10,13 +10,18 @@ const Part = ({ name, exercises }) => (
   </p>
 )
 
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} name={part.name} exercises={part.exercises} />
-    ))}
-  </div>
-)
+const Content = ({ parts }) => {
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+      <b>Total of {totalExercises} exercises</b>
+    </div>
+  )
+}
 
 const Course = ({ course }) => (
   <div>
