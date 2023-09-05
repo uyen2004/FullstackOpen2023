d49c3db6de4fb2645896f743d68e0f6d0ff4dd60
e2000592 <e2000592@vamk.fi>
2023-09-05 22:11:22 +0300

    Exc1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const App = () => {
+ 
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
