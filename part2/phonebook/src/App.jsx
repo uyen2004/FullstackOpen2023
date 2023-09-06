1a1dc58a84f3d4472062febf1dd585c5c2a85ca8
e2000592 <e2000592@vamk.fi>
2023-09-06 12:17:03 +0300

    Exc2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Person from './components/Person';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName 
+    }
+    setPersons([...persons, nameObject])
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}> {}
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, index) => (
+          <Person key={index} person={person} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
