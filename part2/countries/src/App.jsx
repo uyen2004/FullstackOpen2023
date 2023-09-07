2d7f2d3ca1a20e2b3bf604e43b87e62e87a02082
e2000592 <e2000592@vamk.fi>
2023-09-07 13:20:43 +0300

    Exc2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,47 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Country from './component/Country'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Country from './component/Country';
 
 const App = () => {
-  const [query, setQuery] = useState('')
-  const [allCountries, setAllCountries] = useState([])
-  const [filteredCountries, setFilteredCountries] = useState([])
-  const [message, setMessage] = useState('')
+  const [query, setQuery] = useState('');
+  const [allCountries, setAllCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
       .then((response) => {
         if (response.data.length > 0) {
-          setMessage('')
-          setAllCountries(response.data)
+          setMessage('');
+          setAllCountries(response.data);
         } else {
-          setMessage('No countries found.')
-          setAllCountries([])
+          setMessage('No countries found.');
+          setAllCountries([]);
         }
       })
       .catch((error) => {
-        setMessage('Error fetching data.')
-        setAllCountries([])
-      })
-  }, [])
+        setMessage('Error fetching data.');
+        setAllCountries([]);
+      });
+  }, []);
 
   useEffect(() => {
     const filtered = allCountries.filter((country) =>
       country.name.common.toLowerCase().includes(query.toLowerCase())
-    )
+    );
 
     if (filtered.length > 10) {
-      setMessage('Too many matches, specify another filter.')
-      setFilteredCountries([])
+      setMessage('Too many matches, specify another filter.');
+      setFilteredCountries([]);
     } else if (filtered.length >= 1) {
-      setMessage('')
-      setFilteredCountries(filtered)
+      setMessage('');
+      setFilteredCountries(filtered);
     } else {
-      setMessage('No matches found.')
-      setFilteredCountries([])
+      setMessage('No matches found.');
+      setFilteredCountries([]);
     }
-  }, [query, allCountries])
+  }, [query, allCountries]);
 
   return (
     <div>
@@ -56,7 +56,7 @@ const App = () => {
       <p>{message}</p>
       <Country countries={filteredCountries} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/countries/src/component/Country.jsx
+++ part2/countries/src/component/Country.jsx
@@ -1,11 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Country = ({ countries }) => {
   const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    if (selectedCountry) {
+      const apiKey = import.meta.env.VITE_WEATHER_API_KEY
+      const capital = selectedCountry.capital
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`
+
+      axios
+        .get(weatherUrl)
+        .then((response) => {
+          setWeatherData(response.data)
+        })
+        .catch((error) => {
+          console.error('Error fetching weather data:', error)
+        })
+    }
+  }, [selectedCountry])
+
+  useEffect(() => {
+    if (countries.length === 1) {
+      setSelectedCountry(countries[0])
+    }
+  }, [countries])
 
   const showCountryDetails = (country) => {
     setSelectedCountry(country)
-  };
+  }
 
   return (
     <div>
@@ -20,27 +45,29 @@ const Country = ({ countries }) => {
               <li key={index}>{language}</li>
             ))}
           </ul>
-          <img src={selectedCountry.flags.png} alt={`Flag of ${selectedCountry.name.common}`} />
+          {weatherData && (
+            <div>
+              <h3>Weather in {selectedCountry.capital}</h3>
+              <p>Temperature: {weatherData.main.temp}°C</p>
+              {weatherData.weather.length > 0 && (
+                <div>
+                  <h4>Weather Condition:</h4>
+                  <p>{weatherData.weather[0].description}</p>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                    alt="Weather Icon"
+                  />
+                </div>
+              )}
+            </div>
+          )}
         </div>
       ) : (
         countries.map((country) => (
           <div key={country.name.common}>
             <h5>{country.name.common}</h5>
-            {countries.length > 1 ? (
+            {countries.length > 1 && (
               <button onClick={() => showCountryDetails(country)}>Show Details</button>
-            ) : (
-              <div>
-                <h2>{country.name.common}</h2>
-                <p>Capital: {country.capital}</p>
-                <p>Area: {country.area} square kilometers</p>
-                <h3>Languages</h3>
-                <ul>
-                  {Object.values(country.languages).map((language, index) => (
-                    <li key={index}>{language}</li>
-                  ))}
-                </ul>
-                <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
-              </div>
             )}
           </div>
         ))
