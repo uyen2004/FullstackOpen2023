c99c4419b5e52cd02020bb4469d9203058a7c743
e2000592 <e2000592@vamk.fi>
2023-09-10 23:48:19 +0300

    Exc3.11, add production build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -40,6 +40,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next(); // Continue processing the request
+});
+
+
+
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find((p) => p.id === id)
@@ -114,6 +121,7 @@ function generateUniqueId() {
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(express.static('dist'))
 
 const port = 3000
 app.listen(port, '0.0.0.0', () => {
