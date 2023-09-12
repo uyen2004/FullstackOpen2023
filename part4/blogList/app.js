5b801b3ed6ce34a6398af04489943f9629a1c781
e2000592 <e2000592@vamk.fi>
2023-09-12 23:29:05 +0300

    Exc4.2

---
+++ part4/blogList/app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const router = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use('/api/blogs', router)
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+
+module.exports = app
