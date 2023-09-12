c06784f7ff78efe1dfd2f56a36c1369fcb976925
e2000592 <e2000592@vamk.fi>
2023-09-13 00:42:56 +0300

    Exc4.4

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,7 +1,12 @@
-const dummy = (blogs) => {
+  const dummy = (blogs) => {
     return 1
+  }
+
+  const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
   }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
