ec4ad5f38af5c6de691011e1ba49d35a6d8103a1
e2000592 <e2000592@vamk.fi>
2023-09-12 11:51:04 +0300

    Exc3.20

--- part3/phonebook_be/models/person.js
+++ part3/phonebook_be/models/person.js
@@ -3,11 +3,21 @@ const mongoose = require('mongoose')
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 3, 
-    required: true, 
+    minlength: 3,
+    required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (value) {
+        const phoneFormat = /^(?:\d{2,3}-\d+)$/;
+        return phoneFormat.test(value);
+      },
+      message: (props) =>
+        `Please enter phone number in the format XX-XXXXXXX or XXX-XXXXXXXX.`,
+    },
+    required: true,
+  },
 })
 
-
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
