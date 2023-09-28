77fbcfd4ac364043050abcbb49d96c8457803623
e2000592 <e2000592@vamk.fi>
2023-09-29 00:05:00 +0300

    Exc5.10

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,30 +1,30 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseUrl = 'http://localhost:3003/api/blogs'
-let token = null
+const baseUrl = 'http://localhost:3003/api/blogs';
+let token = null;
 
 const setToken = newToken => {
-  token = `Bearer ${newToken}`
-}
+  token = `Bearer ${newToken}`;
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then((response) => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
-const getBlogsByUserId = async (userId) => {
-  const allBlogs = await getAll()
-  return allBlogs.filter((blog) => blog.user && blog.user._id === userId)
-}
+const getBlogsByUserId = async userId => {
+  const allBlogs = await getAll();
+  return allBlogs.filter(blog => blog.user && blog.user._id === userId);
+};
 
 const create = async newBlog => {
   const config = {
     headers: { Authorization: token },
-  }
+  };
 
-  const response = await axios.post(baseUrl, newBlog, config)
-  return response.data
-}
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
 
 const update = async (blogId, updatedBlogData) => {
   try {
@@ -35,4 +35,18 @@ const update = async (blogId, updatedBlogData) => {
   }
 };
 
-export default { getAll, getBlogsByUserId, create, setToken, update }
+const remove = async blogId => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  try {
+    const response = await axios.delete(`${baseUrl}/${blogId}`, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export default { getAll, getBlogsByUserId, create, setToken, update, remove };
