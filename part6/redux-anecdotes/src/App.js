33b48e4a22e36d23823192a5f7b5ba77fde22240
e2000592 <e2000592@vamk.fi>
2023-09-30 01:15:55 +0300

    Exc6.5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,7 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addAnecdote } from './reducers/anecdoteReducer';
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) =>
+    state.sort((a, b) => b.votes - a.votes) 
+  );
   const dispatch = useDispatch();
 
   const vote = (id) => {
@@ -17,7 +19,7 @@ const App = () => {
     e.preventDefault();
     const content = e.target.anecdote.value;
     dispatch(addAnecdote(content));
-    e.target.anecdote.value = ''; 
+    e.target.anecdote.value = '';
   };
 
   return (
