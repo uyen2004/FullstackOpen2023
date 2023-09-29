12328305bc603fbde3eb2d28c9bbe6b918225b42
e2000592 <e2000592@vamk.fi>
2023-09-29 15:45:15 +0300

    Exc5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,13 +1,39 @@
-describe('Blog app', function() {
-  beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/blogs')
-    cy.visit('http://localhost:5173')
-  })
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.visit('http://localhost:5173');
+  });
 
-  it('Login form is shown', function() {
-    cy.get('form').should('exist') 
-    cy.get('input[name="Username"]').should('exist') 
-    cy.get('input[name="Password"]').should('exist')
-    cy.get('button[type="submit"]').should('exist') 
-  })
-})
+  it('Login form is shown', function () {
+    cy.get('form').should('be.visible');
+    cy.get('input[name="Username"]').should('be.visible');
+    cy.get('input[name="Password"]').should('be.visible');
+    cy.get('button[type="submit"]').should('be.visible');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.request('POST', 'http://localhost:3003/api/users', {
+        username: 'trish',
+        password: 'trish123',
+        name: 'trisha',
+      });
+
+      cy.visit('http://localhost:5173/login');
+      cy.get('input[name="Username"]').type('trish');
+      cy.get('input[name="Password"]').type('trish123');
+      cy.get('button[type="submit"]').click();
+
+      cy.contains('trish logged in');
+      cy.contains('Logout');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('input[name="Username"]').type('incorrectusername');
+      cy.get('input[name="Password"]').type('incorrectpassword');
+      cy.get('button[type="submit"]').click();
+
+      cy.get('.error').should('contain', 'Wrong username or password');
+    });
+  });
+});
