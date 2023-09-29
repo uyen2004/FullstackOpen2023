5ceb11b87697b0a30b2a6c57706aea66bb02a051
e2000592 <e2000592@vamk.fi>
2023-09-29 16:11:54 +0300

    Exc5.19

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -35,5 +35,58 @@ describe('Blog app', function () {
 
       cy.get('.error').should('contain', 'Wrong username or password');
     });
+  
+    describe('When logged in', function() {
+      beforeEach(function() {
+        cy.request('POST', 'http://localhost:3003/api/users', {
+          username: 'trish',
+          password: 'trish123',
+          name: 'Trisha',
+        });
+        cy.visit('http://localhost:5173/login');
+        cy.get('input[name="Username"]').type('trish');
+        cy.get('input[name="Password"]').type('trish123');
+        cy.get('button[type="submit"]').click();
+      });
+  
+      it('Shows the user info and "Create New Blog" button', function() {
+        cy.contains('Blogs');
+        cy.contains('trish logged in');
+        cy.contains('Logout');
+        cy.contains('No blogs found.');
+  
+        cy.get('button').contains('Create New Blog').should('be.visible');
+      });
+  
+      it('A blog can be created', function() {
+        cy.contains('Create New Blog').click();
+        cy.get('input[name="title"]').type('Test Blog');
+        cy.get('input[name="author"]').type('John Doe');
+        cy.get('input[name="url"]').type('https://example.com/test-blog');
+        cy.get('button[type="submit"]').click();
+  
+        cy.contains('Test Blog');
+        cy.contains('John Doe');
+      });
+    });
+  })
+
+  describe('Liking a Blog', function () {
+    beforeEach(function () {
+      cy.request('POST', 'http://localhost:3003/api/users', {
+          username: 'trish',
+          password: 'trish123',
+          name: 'Trisha',
+        });
+        cy.visit('http://localhost:5173/login');
+        cy.get('input[name="Username"]').type('trish');
+        cy.get('input[name="Password"]').type('trish123');
+        cy.get('button[type="submit"]').click();
+    });
+  
+    it('User can like a blog', function () {
+      cy.get('.blog').contains('Like').click();
+      cy.get('.blog').contains('Likes: 1');
+    });
   });
 });
